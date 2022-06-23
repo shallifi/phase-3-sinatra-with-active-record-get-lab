0839a43a78631450db17206426ef4b2928c211e5
Benjamin <shallifi@hotmail.com>
2022-06-22 23:37:42 -0400

    completed assignment

--- app/models/baked_good.rb
+++ app/models/baked_good.rb
@@ -1,3 +1,15 @@
 class BakedGood < ActiveRecord::Base
   # add association macro here
+  belongs_to :bakery
+
+
+  def self.by_price
+    all.order(price: :desc)
+  end
+
+  def self.most_expensive
+    all.order(price: :desc).first
+  end
+
+
 end
